bff301633ab7a9922fe359bb89b35941c3344218
axross <kou@axross.org>
2015-08-23 02:30:23 +0900

    Add a simple database

--- data/database.js
+++ data/database.js
@@ -1,33 +1,36 @@
-/**
- *  Copyright (c) 2015, Facebook, Inc.
- *  All rights reserved.
- *
- *  This source code is licensed under the BSD-style license found in the
- *  LICENSE file in the root directory of this source tree. An additional grant
- *  of patent rights can be found in the PATENTS file in the same directory.
- */
-
 // Model types
-class User extends Object {}
-class Widget extends Object {}
+class Game extends Object {}
+class HidingSpot extends Object {}
 
 // Mock data
-var viewer = new User();
-viewer.id = '1';
-viewer.name = 'Anonymous';
-var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
-  var widget = new Widget();
-  widget.name = name;
-  widget.id = `${i}`;
-  return widget;
-});
+let game = new Game();
+game.id = '1';
 
-module.exports = {
-  // Export methods that your schema can use to interact with your database
-  getUser: (id) => id === viewer.id ? viewer : null,
-  getViewer: () => viewer,
-  getWidget: (id) => widgets.find(w => w.id === id),
-  getWidgets: () => widgets,
-  User,
-  Widget,
+let hidingSpots = [];
+() => {
+  let hidingSpot;
+  const indexOfSpotWithTreasure = Math.floor(Math.random() * 9);
+  for (let i = 0; i < 9; ++i) {
+    hidingSpot = new HidingSpot();
+    hidingSpot.id = `${i}`;
+    hidingSpot.hasTreasure = (i === indexOfSpotWithTreasure);
+    hidingSpot.hasBeenChecked = false;
+    hidingSpots.push(hidingSpot);
+  }
 };
+
+let turnsRemaining = 3;
+
+export const checkHidingSpotForTreasure = id => {
+  if (hidingSpots.some(hs => hs.hasTreasure && hs.hasBeenChecked)) return;
+
+  --turnsRemaining;
+
+  let hidingSpot = getHidingSpot(id);
+  hidingSpot.hasBeenChecked = true;
+};
+
+export const getHidingSpot = id => hidingSpots.find(hs => hs.id === id);
+export const getGame = () => game;
+export const getHidingSpots = () => hidingSpots;
+export const getTurnsRemaining = () => turnsRemaining;
